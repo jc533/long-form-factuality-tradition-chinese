5317e0355af0ebee81b9eafc551738dd2533b5db
Justin Chen <yub612@gmail.com>
2024-10-31 16:41:11 +0800

    prompt ver 1

--- eval/safe/rate_atomic_fact.py
+++ eval/safe/rate_atomic_fact.py
@@ -40,7 +40,9 @@ will allow you to find additional useful evidence.
 4. Your query should aim to obtain new information that does not appear in the \
 KNOWLEDGE. This new information should be useful for determining the factual \
 accuracy of the given STATEMENT.
-5. Format your final query by putting it in a markdown code block.
+5. The language you use in the query must be in consistent with the major language \
+of the statement.
+6. Format your final query by putting it in a markdown code block.
 
 KNOWLEDGE:
 {_KNOWLEDGE_PLACEHOLDER}
