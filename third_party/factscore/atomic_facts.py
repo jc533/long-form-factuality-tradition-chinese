b35a626f64465fe5a5dc956feb8c7a82c9a26e7c
Justin Chen <yub612@gmail.com>
2024-11-15 01:29:53 +0800

    tw choose 1

--- third_party/factscore/atomic_facts.py
+++ third_party/factscore/atomic_facts.py
@@ -60,10 +60,11 @@ list of atomic facts.
 2. An atomic fact is a sentence containing a singular piece of information.
 3. Each atomic fact in the outputted list should check a different piece of \
 information.
-4. Use the previous examples to learn how to do this.
-5. You should only output the atomic facts as a list, with each item starting \
+4. The language of atomic fact must be the same as the given sentence.
+5. Use the previous examples to learn how to do this.
+6. You should only output the atomic facts as a list, with each item starting \
 with "- ". Do not include other formatting.
-6. Your task is to do this for the last sentence that is given.
+7. Your task is to do this for the last sentence that is given.
 """
 
 
